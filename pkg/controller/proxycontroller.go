dba238d417ef62eb4026582f61b891f4873a6a6b
Todd Short <todd.short@me.com>
2024-12-04 16:21:23 -0500

    Don't use var
    Signed-off-by: Todd Short <todd.short@me.com>

--- pkg/controller/proxycontroller.go
+++ pkg/controller/proxycontroller.go
@@ -87,7 +87,7 @@ func UpdateProxyEnvironment(logger logr.Logger, pc *clients.ProxyClient) (bool, 
 		return false, fmt.Errorf("failed to convert object to unstructured: %+v: %w", objSpec, err)
 	}
 	logger.Info("converting unstructured to proxy")
-	var proxySpec &configv1.Proxy{}
+	proxySpec := &configv1.Proxy{}
 	err = runtime.DefaultUnstructuredConverter.FromUnstructured(uns, proxySpec)
 	if err != nil {
 		return false, fmt.Errorf("failed to convert unstructured to proxy: %+v: %w", uns, err)
